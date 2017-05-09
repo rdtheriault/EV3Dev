84f75d86ef1c24a121994ae4b51a6e1f9d7292c5
Robert <rdtheriault@yahoo.com>
2017-05-09 07:32:04 -0700

    Update testPI.py
    Changed to ev3 sensor

--- testPI.py
+++ testPI.py
@@ -1,6 +1,6 @@
 import subprocess
-subprocess.call("echo ev3-analog > /sys/class/lego-port/port0/mode", shell=True)
-subprocess.call("echo lego-nxt-touch > /sys/class/lego-port/port0/set_device", shell=True)
+subprocess.call("echo ev3-analog > /sys/class/lego-port/port1/mode", shell=True)
+subprocess.call("echo lego-ev3-touch > /sys/class/lego-port/port1/set_device", shell=True)
 
 from ev3dev.ev3 import *
 from time import sleep
@@ -8,14 +8,16 @@ from time import sleep
 m = LargeMotor('ttyAMA0:MA')
 
 
-ts = TouchSensor('ttyAMA0:S1')
+ts = TouchSensor('ttyAMA0:S2')
 assert ts.connected, "Connect TouchSensor"
 
 notTouched = 1
 
+m.run_forever(speed_sp=300)
+print(ts.value())
+sleep(3)
 while notTouched == 1:
-
-        m.run_forever(speed_sp=300)
-        if ts.value() == 1:
-                m.stop()
-                notTouched == 0
+ if ts.value() == 1:
+  print(ts.value())
+  m.stop()
+  notTouched = 0
